e98a7755125525d2bff3bde6a462f84e9dba9b21
tsumo <tsumo@users.noreply.github.com>
2021-03-09 12:17:23 +0300

    chore: webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,17 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
       title: "CG UC Davis",
     }),
   ],
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
