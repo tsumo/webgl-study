9d63b98a38762c68de0e772aae73b58225f02436
tsumo <tsumo@users.noreply.github.com>
2021-03-09 14:17:44 +0300

    chore: eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import "./style.css";
+import './style.css';
 
-const init = () => {
-  const canvas = document.createElement("canvas");
+const init = (): void => {
+  const canvas = document.createElement('canvas');
   document.body.appendChild(canvas);
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext('2d');
   if (ctx === null) {
-    console.error("Cannot get canvas context");
+    console.error('Cannot get canvas context');
     return;
   }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.ts",
+  mode: 'development',
+  entry: './src/index.ts',
   plugins: [
     new HtmlWebpackPlugin({
-      title: "CG UC Davis",
+      title: 'CG UC Davis',
     }),
   ],
-  devtool: "eval-source-map",
+  devtool: 'eval-source-map',
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
 };
