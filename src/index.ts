b6d44f2e6fc069428a16bb783daa01581efe5c71
tsumo <tsumo@users.noreply.github.com>
2021-03-09 13:19:08 +0300

    chore: typescript

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+type Test = { a: string; b: boolean };
+
+const test: Test = { a: "", b: false };
+
+console.log(test);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   plugins: [
     new HtmlWebpackPlugin({
       title: "CG UC Davis",
@@ -21,9 +21,17 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
     ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
   },
 };
