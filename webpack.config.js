7457c959ab2f6c2954ad9a884008938d876d0cff
tsumo <tsumo@users.noreply.github.com>
2021-03-09 11:59:32 +0300

    chore: html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "CG UC Davis",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
