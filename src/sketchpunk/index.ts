99a745b7dcbb284991be268ac43aa1df410bd460
tsumo <tsumo@users.noreply.github.com>
2021-03-31 15:14:56 +0300

    feat: simple math animations

--- src/sketchpunk/index.ts
+++ src/sketchpunk/index.ts
@@ -28,6 +28,7 @@ const init = (): void => {
     fragmentShaderSource,
     {
       uPointSize: { type: 'float', value: 50 },
+      uAngle: { type: 'float', value: 0 },
       uPointColor: { type: 'vec4', value: [1, 1, 1, 1] },
     },
     true,
@@ -43,22 +44,32 @@ const init = (): void => {
 
   const gui = new Gui(
     {
-      uPointSize: { default: 50, min: 0, max: 300, step: 0.01 },
+      basePointSize: { default: 50, min: 0, max: 300, step: 0.01 },
       red: { default: rand(), min: 0, max: 1, step: 0.01 },
       green: { default: rand(), min: 0, max: 1, step: 0.01 },
       blue: { default: rand(), min: 0, max: 1, step: 0.01 },
     },
     (values) => {
-      program.setUniform('uPointSize', values.uPointSize);
+      program.setUniform('uPointSize', values.basePointSize);
       program.setUniform('uPointColor', [values.red, values.green, values.blue, 1]);
     },
   );
 
+  let animatedPointSize = 0;
+  let angle = 0;
+
   const tick: TickFunction = (delta, fps): void => {
+    animatedPointSize += delta * 3;
+    const finalPointSize = Math.sin(animatedPointSize) * 10 + gui.guiValues.basePointSize;
+
+    angle = (angle + delta * 0.5) % (Math.PI * 2);
+
     fpsElement.innerText = String(fps);
+
     canvas.clear();
     program.use();
-    program.setUniform('uPointSize', gui.guiValues.uPointSize);
+    program.setUniform('uPointSize', finalPointSize);
+    program.setUniform('uAngle', angle);
     program.setUniform('uPointColor', [
       gui.guiValues.red,
       gui.guiValues.green,
--- src/sketchpunk/vertex.glsl
+++ src/sketchpunk/vertex.glsl
@@ -3,8 +3,14 @@
 in vec3 a_position;
 
 uniform float uPointSize;
+uniform float uAngle;
 
 void main(void) {
   gl_PointSize = uPointSize;
-  gl_Position = vec4(a_position, 1.0);
+  gl_Position = vec4(
+    cos(uAngle) * 0.8 + a_position.x,
+    sin(uAngle) * 0.8 + a_position.y,
+    a_position.z,
+    1.0
+  );
 }
