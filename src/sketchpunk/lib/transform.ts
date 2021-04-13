66d63431ecb49cea42f588639ea7950b088d25d0
tsumo <tsumo@users.noreply.github.com>
2021-04-13 10:02:54 +0300

    feat: transform3d class

--- src/sketchpunk/lib/transform.ts
+++ src/sketchpunk/lib/transform.ts
@@ -1,4 +1,4 @@
-import { mat3, vec2 } from 'gl-matrix';
+import { mat3, mat4, quat, vec2, vec3 } from 'gl-matrix';
 
 // TODO: addTranslation/Rotation/Scale methods
 // TODO: automatically calculate up/right vectors
@@ -28,6 +28,41 @@ export class Transform2d {
     mat3.mul(this.matrix, this.matrix, this.translationMatrix);
     mat3.mul(this.matrix, this.matrix, this.rotationMatrix);
     mat3.mul(this.matrix, this.matrix, this.scaleMatrix);
+    return this.matrix;
+  }
+}
+
+export class Transform3d {
+  private translationMatrix = mat4.create();
+  private rotationQuat = quat.create();
+  private rotationMatrix = mat4.create();
+  private scaleMatrix = mat4.create();
+  private matrix = mat4.create();
+
+  setTranslation(translation: vec3): void {
+    mat4.fromTranslation(this.translationMatrix, translation);
+  }
+
+  /**
+   * @param rotation in degrees
+   */
+  setRotation(rotation: vec3): void {
+    quat.fromEuler(this.rotationQuat, rotation[0], rotation[1], rotation[2]);
+    mat4.fromQuat(this.rotationMatrix, this.rotationQuat);
+  }
+
+  setScale(scale: vec3): void {
+    mat4.fromScaling(this.scaleMatrix, scale);
+  }
+
+  reset(): void {
+    mat4.identity(this.matrix);
+  }
+
+  update(): mat4 {
+    mat4.mul(this.matrix, this.matrix, this.translationMatrix);
+    mat4.mul(this.matrix, this.matrix, this.rotationMatrix);
+    mat4.mul(this.matrix, this.matrix, this.scaleMatrix);
     return this.matrix;
   }
 }
