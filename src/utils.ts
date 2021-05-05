492a979770ef12f92d08ea2308fe88dd62c30e8b
tsumo <tsumo@users.noreply.github.com>
2021-05-05 17:33:30 +0300

    feat(utils): polar and spherical coordinates

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
-const { random, floor, PI } = Math;
+import { vec2, vec3 } from 'gl-matrix';
+
+const { random, floor, PI, sin, cos, sqrt, atan2, acos } = Math;
 
 export const assertUnreachable = (value: never): never => {
   console.warn(value);
@@ -26,3 +28,16 @@ const rad2degPreCalc = 180 / PI;
 export const rad2deg = (r: number): number => r * rad2degPreCalc;
 
 export const lerp = (start: number, end: number, t: number): number => start * (1 - t) + end * t;
+
+export const cart2polar = (c: vec2): vec2 => [sqrt(c[0] * c[0] + c[1] * c[1]), atan2(c[1], c[0])];
+export const polar2cart = (p: vec2): vec2 => [p[0] * cos(p[1]), p[0] * sin(p[1])];
+
+export const cart2spher = (c: vec3): vec3 => {
+  const rho = sqrt(c[0] * c[0] + c[1] * c[1] + c[2] * c[2]);
+  return [rho, acos(c[2] / rho), atan2(c[1], c[0])];
+};
+export const spher2cart = (s: vec3): vec3 => [
+  s[0] * sin(s[1]) * cos(s[2]),
+  s[0] * sin(s[1]) * sin(s[2]),
+  s[0] * cos(s[1]),
+];
