683bac95a497e0c74c59720c1cab96387c92ed8f
tsumo <tsumo@users.noreply.github.com>
2021-05-10 00:08:20 +0300

    build: webpack prod and dev configs

--- webpack.common.js
+++ webpack.common.js
@@ -2,17 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.ts',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'WebGL study',
     }),
   ],
-  devtool: 'eval-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
